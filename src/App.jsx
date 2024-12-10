b4674829e01405f863f87c93886132ca6f932229
Carlos CaravanTsz <carloscara28@hotmail.com>
2024-12-10 00:26:46 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { GlobalStyles } from './index'
+
+function App() {
+
+  return (
+    <div>
+      <GlobalStyles />
+      <span>hola mundo</span>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as App } from './App.jsx'
+export * from './main'
+export * from './styles/GlobalStyles'
+
